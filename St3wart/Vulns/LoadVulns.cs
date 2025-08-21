59d3e1465c14164f754a8a32737ddc8b5e150625
JohnDavid Abe <johndavidabe101@gmail.com>
2025-08-21 05:20:16 -0700

    Touched up LoadVulns

--- St3wart/Vulns/LoadVulns.cs
+++ St3wart/Vulns/LoadVulns.cs
@@ -1,7 +1,7 @@
 /*
 
 Stewart CLI
-/Vulns/LoadVulns.cs - Load vulns from a file
+/Vulns/LoadVulns.cs - Load vulns from a JSON vuln database
 JohnDavid Abe 
 
 */
@@ -14,56 +14,94 @@ using Newtonsoft.Json.Linq;
 
 
 
-public static class DataHandler
-{
-    public static Dictionary<string, Check> VulnsFromFile(string filePath)
-    {
-        string jsonFromFile = File.ReadAllText(filePath);
+/// <summary>
+/// Handles retrieving data from a JSON vuln database
+/// </summary>
+public static class DataHandler {
 
-        var settings = new JsonSerializerSettings
-        {
-            Converters = { new CheckConverter() }
-        };
-
-        var checks = JsonConvert.DeserializeObject<List<Check>>(jsonFromFile, settings);
-
+    /// <summary>
+    /// Loads data from a JSON vuln database
+    /// </summary>
+    /// <param name="filePath">Path to the JSON file</param>
+    /// <returns>
+    /// A dictionary of ID/Check key/value pairs
+    /// </returns>
+    public static Dictionary<string, Check> VulnsFromFile(string filePath) {
+        
+        // Create a dictionary of the Checks where the Check ID the key and the Check itself is the value 
         var dict = new Dictionary<string, Check>();
-        foreach (var check in checks)
-        {
-            dict[check.ID] = check;
-        }
 
+        try {
+
+            // Read the entire file
+            string jsonFromFile = File.ReadAllText(filePath);
+            if (jsonFromFile == null) { return dict; }
+
+            // Compile the JSON into a list of Checks
+            var settings = new JsonSerializerSettings {
+                Converters = { new CheckConverter() }
+            };
+            var checks = JsonConvert.DeserializeObject<List<Check>>(jsonFromFile, settings);
+            if (checks == null) { return dict;  }
+            
+            // Loop through the Check from JSON and compile a dict using the ID's of each Check
+            foreach (var check in checks) {
+                if(check.ID != null) {
+                    dict[check.ID] = check;
+                }
+            }
+            
+        } catch { }
+
+        // Return the compiled dict
         return dict;
     }
 
-    private class CheckConverter : JsonConverter
-    {
+
+
+    /// <summary>
+    /// Custom JSON Converter for deseralizing JSON data
+    /// </summary>
+    private class CheckConverter : JsonConverter {
+
+        // Override to set the object to deseralize to type Check
         public override bool CanConvert(Type objectType) => objectType == typeof(Check);
+        
+        // Override to change how deseralized JSON data is handled
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) {
 
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-            JObject jo = JObject.Load(reader);
-            string checkType = jo["CheckType"]?.ToString();
-
-            Check check;
-            switch (checkType)
-            {
-                case "PowerShell":
-                    check = new PowerShellCheck();
-                    break;
-                case "Registry":
-                    check = new RegistryCheck();
-                    break;
-                default:
-                    throw new NotSupportedException($"Unknown CheckType: {checkType}");
-            }
-
-            serializer.Populate(jo.CreateReader(), check);
-            return check;
+            try {
+                // Load the current JSON object
+                JObject jo = JObject.Load(reader);
+                if (jo == null) { return new Check(); }
+                
+                // Get the type of the check
+                string? checkType = jo["CheckType"]?.ToString();
+                
+                // Assign accordingly to the new Check object
+                Check check;
+                switch (checkType) {
+                    case "PowerShell":
+                        check = new PowerShellCheck();
+                        break;
+                    case "Registry":
+                        check = new RegistryCheck();
+                        break;
+                    default:
+                        // Return an empty, general Check if the type of the Check is unidentified
+                        return new Check();
+                }
+                
+                // Populate the particular Check object and return it
+                serializer.Populate(jo.CreateReader(), check);
+                return check;
+            
+            // Return an empty, general Check if any errors occur
+            } catch { return new Check();  }
         }
 
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        {
+        // Override to write the object as JSON
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
             serializer.Serialize(writer, value);
         }
     }

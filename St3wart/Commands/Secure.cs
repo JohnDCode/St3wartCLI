6efccceb09d6328711f5164f8062a09c5c434d1b
JohnDavid Abe <johndavidabe101@gmail.com>
2025-08-22 22:27:52 -0700

    backup before start work on secure

--- St3wart/Commands/Secure.cs
+++ St3wart/Commands/Secure.cs
@@ -14,3 +14,18 @@ public class SecureCommand : ICommand
     public async Task Execute(string[] args) { } 
     public void Help() { }
 }
+
+
+
+/*
+
+Idk how to implement SecureCommand
+
+FIrst of all, have it secure by GUID
+Command parameters will specify a GUID
+
+Load all vulns that were marked as findings
+
+Will need tm modify all current runners to not just execute checks but also be able to just run secure / modify data instead of just reading it (will be easiest for powershell, hardest for files but not that hard)
+
+*/
